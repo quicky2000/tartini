0ccfb258e63beec06bb593d5990a6724eb4a2f79
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-05-25 17:39:42 +0200

    Add missing file headers

--- general/fast_smooth.h
+++ general/fast_smooth.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                           fast_smooth.h
+                           -------------------
+    begin                : Feb 2005
+    copyright            : (C) 2005 by Philip McLeod
+    email                : pmcleod@cs.otago.ac.nz
+    copyright            : (C) 2016 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Please read LICENSE.txt for details.
+ ***************************************************************************/
 #ifndef _FAST_SMOOTH_H
 #define _FAST_SMOOTH_H
 
