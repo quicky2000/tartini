cf34b3234c4dbb308587150e8257e02864105773
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-05-30 08:54:10 +0200

    Dummy

--- sound/sound_stream.h
+++ sound/sound_stream.h
@@ -4,6 +4,8 @@
     begin                : Sat Nov 27 2004
     copyright            : (C) 2004-2005 by Philip McLeod
     email                : pmcleod@cs.otago.ac.nz
+    copyright            : (C) 2016 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
  
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
