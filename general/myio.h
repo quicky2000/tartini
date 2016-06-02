76804556c735b98994ba090ef9b2586f9e058cf1
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-06-02 22:58:11 +0200

    Cosmetic changes

--- general/myio.h
+++ general/myio.h
@@ -4,23 +4,59 @@
     begin                : Unknown
     copyright            : Unknown
     email                : Unknown
- 
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    Please read LICENSE.txt for details.
  ***************************************************************************/
+#ifndef MY_IO_H
+#define MY_IO_H
+
 #include <stdio.h>
 
-int igetw(FILE *f);  //Reads a 16 bit word (intel byte ordering)
-long igetl(FILE *f); //Reads a 32 bit long (intel byte ordering)
-int iputw(int w, FILE *f); //Writes a 16 bit int (intel byte ordering)
-long iputl(long l, FILE *f); //Writes a 32 bit long (intel byte ordering)
+/**
+   Reads a 16 bit word (intel byte ordering)
+*/
+int igetw(FILE *f);
 
+/**
+   Reads a 32 bit long (intel byte ordering)
+*/
+long igetl(FILE *f);
 
-int mgetw(FILE *f); //Reads a 16 bit int (motorola byte-ordering)
-long mgetl(FILE *f); //Reads a 32 bit long (motorola byte-ordering)
-int mputw(int w, FILE *f); //Writes a 16 bit int (motorola byte-ordering)
-long mputl(long l, FILE *f); //Writes a 32 bit long (motorola byte-ordering)
+/**
+   Writes a 16 bit int (intel byte ordering)
+*/
+int iputw(int w, FILE *f);
+
+/**
+   Writes a 32 bit long (intel byte ordering)
+*/
+long iputl(long l, FILE *f);
+
+/**
+   Reads a 16 bit int (motorola byte-ordering)
+*/
+int mgetw(FILE *f);
+
+/**
+   Reads a 32 bit long (motorola byte-ordering)
+*/
+long mgetl(FILE *f);
+
+/**
+   Writes a 16 bit int (motorola byte-ordering)
+*/
+int mputw(int w, FILE *f);
+
+/**
+   Writes a 32 bit long (motorola byte-ordering)
+*/
+long mputl(long l, FILE *f);
+
+#endif // MY_IO_H
+
+//EOF
