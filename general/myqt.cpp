eb504c263f24d271b7ad7d71253e79e8b067e02c
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-06-03 08:30:41 +0200

    Cosmetic changes

--- general/myqt.cpp
+++ general/myqt.cpp
@@ -1,14 +1,38 @@
+/***************************************************************************
+                          myqt.cpp
+                             -------------------
+    begin                : Unknown
+    copyright            : Unknown
+    email                : Unknown
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Please read LICENSE.txt for details.
+ ***************************************************************************/
 #include "myqt.h"
 
-/** Gives a color between a and b, using ratio (a number between 0 and 1)
+/**
+   Gives a color between a and b, using ratio (a number between 0 and 1)
 */
 QColor colorBetween(QColor a, QColor b, double ratio)
 {
   //myassert(ratio >= 0.0 && ratio <= 1.0);
-  if(ratio < 0.0) ratio = 0.0;
-  if(ratio > 1.0) ratio = 1.0;
+  if(ratio < 0.0)
+    {
+      ratio = 0.0;
+    }
+  if(ratio > 1.0)
+    {
+      ratio = 1.0;
+    }
   return QColor(
-    a.red()+int(ratio*float(b.red()-a.red())),
-    a.green()+int(ratio*float(b.green()-a.green())),
-    a.blue()+int(ratio*float(b.blue()-a.blue())));
+		a.red()+int(ratio*float(b.red()-a.red())),
+		a.green()+int(ratio*float(b.green()-a.green())),
+		a.blue()+int(ratio*float(b.blue()-a.blue()))
+		);
 }
+
+//EOF
--- general/myqt.h
+++ general/myqt.h
@@ -1,10 +1,27 @@
+/***************************************************************************
+                          myqt.h
+                             -------------------
+    begin                : Unknown
+    copyright            : Unknown
+    email                : Unknown
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   Please read LICENSE.txt for details.
+ ***************************************************************************/
 #ifndef MYQT_H
 #define MYQT_H
 
 #include <QColor>
 
-/** Gives a color between a and b, using ratio (a number between 0 and 1)
+/**
+   Gives a color between a and b, using ratio (a number between 0 and 1)
 */
 extern QColor colorBetween(QColor a, QColor b, double ratio);
 
-#endif
+#endif // MYQT_H
+// EOF
+
