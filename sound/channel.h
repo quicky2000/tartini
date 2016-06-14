bffb5d0dbf5c8bc75910c4c70bc97bbd3fec55d0
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-06-14 12:56:26 +0200

    Make nsdfAggregateRoof member of Channel class private for better data encapsulation

--- sound/channel.h
+++ sound/channel.h
@@ -226,8 +226,12 @@ class Channel
   Array1d<float> nsdfAggregateData;
   Array1d<float> nsdfAggregateDataScaled;
 
+  /**
+     Keeps the sum of scalers. i.e. The highest possible aggregate value
+  */
+  double nsdfAggregateRoof;
+
  public:
-  double nsdfAggregateRoof; //keeps the sum of scalers. i.e. The highest possible aggregate value
   Array1d<float> fftData1;
   Array1d<float> fftData2;
   Array1d<float> fftData3;
