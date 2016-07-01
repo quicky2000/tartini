0a4f7721c94e7689921c4a7c036ae0c289612e1b
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-07-01 08:33:47 +0200

    cosmetic changes

--- widgets/timeaxis.cpp
+++ widgets/timeaxis.cpp
@@ -4,6 +4,8 @@
     begin                : Mon Dec 13 2004
     copyright            : (C) 2004-2005 by Philip McLeod
     email                : pmcleod@cs.otago.ac.nz
+    copyright            : (C) 2016 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
  
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,18 +23,19 @@
 #include <qpainter.h>
 #include <QPaintEvent>
 
+//------------------------------------------------------------------------------
 TimeAxis::TimeAxis(QWidget *parent, bool numbersOnTop_)
   : DrawWidget(parent)
 {
   _leftTime = 0.0;
   _rightTime = 0.0;
-  _numbersOnTop = numbersOnTop_;
-  
+  _numbersOnTop = numbersOnTop_; 
   init();
 }
 
-TimeAxis::TimeAxis(QWidget *parent, double leftTime_, double rightTime_, bool numbersOnTop_)
-  : DrawWidget(parent)
+//------------------------------------------------------------------------------
+TimeAxis::TimeAxis(QWidget *parent, double leftTime_, double rightTime_, bool numbersOnTop_):
+  DrawWidget(parent)
 {
   _leftTime = leftTime_;
   _rightTime = rightTime_;
@@ -40,7 +43,8 @@ TimeAxis::TimeAxis(QWidget *parent, double leftTime_, double rightTime_, bool nu
   init();
 }
 
-void TimeAxis::init()
+//------------------------------------------------------------------------------
+void TimeAxis::init(void)
 {
   setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed, false));
 
@@ -51,98 +55,141 @@ void TimeAxis::init()
 #endif
 }
 
-TimeAxis::~TimeAxis()
+//------------------------------------------------------------------------------
+TimeAxis::~TimeAxis(void)
 {
 }
 
+//------------------------------------------------------------------------------
 void TimeAxis::setFontSize(int fontSize)
 {
   _fontSize = fontSize;
   _font = QFont("AnyStyle", _fontSize);
 }
 
+//------------------------------------------------------------------------------
 void TimeAxis::paintEvent(QPaintEvent *)
 {
   int frameWidth = 2;
-  const int	h = height(), w = width() - 2*frameWidth;
-  int fontSpace = _fontSize+2;
+  const int h = height();
+  const int w = width() - 2 * frameWidth;
+  int fontSpace = _fontSize + 2;
   
   beginDrawing(false);
   fillBackground(colorGroup().background());
 
-  double timeStep = timeWidth() / double(w) * 150.0; //time per 150 pixels
-  double timeScaleBase = pow10(floor(log10(timeStep))); //round down to the nearest power of 10
+  //time per 150 pixels
+  double timeStep = timeWidth() / double(w) * 150.0;
+
+  //round down to the nearest power of 10
+  double timeScaleBase = pow10(floor(log10(timeStep)));
 
   //choose a timeScaleStep which is a multiple of 1, 2 or 5 of timeScaleBase
   int largeFreq;
-  if(timeScaleBase * 5.0 < timeStep) { largeFreq = 5; }
-  else if (timeScaleBase * 2.0 < timeStep) { largeFreq = 2; }
-  else { largeFreq = 2; timeScaleBase /= 2; }
+  if(timeScaleBase * 5.0 < timeStep)
+    {
+      largeFreq = 5;
+    }
+  else if (timeScaleBase * 2.0 < timeStep)
+    {
+      largeFreq = 2;
+    }
+  else
+    {
+      largeFreq = 2;
+      timeScaleBase /= 2;
+    }
     
   // Draw Ruler Numbers
   p.setBrush(Qt::black);
   p.setFont(_font);
-  double timePos = floor(leftTime() / (timeScaleBase*largeFreq)) * (timeScaleBase*largeFreq); //calc the first one just off the left of the screen
-  int x, largeCounter=-1;
+  //calc the first one just off the left of the screen
+  double timePos = floor(leftTime() / (timeScaleBase * largeFreq)) * (timeScaleBase * largeFreq);
+  int x, largeCounter = -1;
   
   //precalculate line sizes (for efficiency)
   int smallLineTop = 0;
   int smallLineBottom = 0;
-  if(_numbersOnTop) {
-    smallLineTop = h - 1 - (h - 1 - fontSpace)/2;
-    smallLineBottom = h - 1;
-  } else {
-    smallLineTop = 0;
-    smallLineBottom = (h - 1 - fontSpace) / 2;
-  }
+  if(_numbersOnTop)
+    {
+      smallLineTop = h - 1 - (h - 1 - fontSpace)/2;
+      smallLineBottom = h - 1;
+    }
+  else
+    {
+      smallLineTop = 0;
+      smallLineBottom = (h - 1 - fontSpace) / 2;
+    }
   int bigLineTop = 0;
   int bigLineBottom = 0;
-  if(_numbersOnTop) {
-    bigLineTop = fontSpace;
-    bigLineBottom = h - 1;
-  } else {
-    bigLineTop = 0;
-    bigLineBottom = h - 1 - fontSpace;
-  }
-  int textBottom = 0;
-  if(_numbersOnTop) textBottom = _fontSize;
-  else textBottom = h - 1;
-    
-  for(; timePos <= rightTime(); timePos += timeScaleBase) {
-    if(++largeCounter == largeFreq) {
-      largeCounter = 0;
-      //draw the bigger lines and the numbers
-      double newTime = myround(timePos / timeScaleBase) * timeScaleBase;
-      QString mins;
-      double secs = fmod(newTime, 60.0);
-
-      if (timePos < 0) {
-        mins = "-" + QString::number(int(ceil(newTime / 60)));
-        secs *= -1;
-      } else {
-        mins = QString::number(int(floor(newTime / 60)));
-      }
-
-      QString seconds = QString::number(secs);
-      if (secs < 10 && secs > -10) {
-        seconds = "0" + seconds;
-      }
-      
-      QString numString = mins + ":" + seconds;
-      x = frameWidth + toInt((timePos-leftTime()) / (timeWidth() / double(w)));
-      p.drawText(x - (p.fontMetrics().width(numString) / 2), textBottom, numString);
-      p.drawLine(x, bigLineTop, x, bigLineBottom);
-    } else {
-      //draw the smaller lines
-      x = frameWidth + toInt((timePos-leftTime()) / (timeWidth() / double(w)));
-      p.drawLine(x, smallLineTop, x, smallLineBottom);
+  if(_numbersOnTop)
+    {
+      bigLineTop = fontSpace;
+      bigLineBottom = h - 1;
     }
-  }
+  else
+    {
+      bigLineTop = 0;
+      bigLineBottom = h - 1 - fontSpace;
+    }
+  int textBottom = 0;
+  if(_numbersOnTop)
+    {
+      textBottom = _fontSize;
+    }
+  else
+    {
+      textBottom = h - 1;
+    }
+    
+  for(; timePos <= rightTime(); timePos += timeScaleBase)
+    {
+      if(++largeCounter == largeFreq)
+	{
+	  largeCounter = 0;
+	  //draw the bigger lines and the numbers
+	  double newTime = myround(timePos / timeScaleBase) * timeScaleBase;
+	  QString mins;
+	  double secs = fmod(newTime, 60.0);
+
+	  if (timePos < 0)
+	    {
+	      mins = "-" + QString::number(int(ceil(newTime / 60)));
+	      secs *= -1;
+	    }
+	  else
+	    {
+	      mins = QString::number(int(floor(newTime / 60)));
+	    }
+
+	  QString seconds = QString::number(secs);
+	  if (secs < 10 && secs > -10)
+	    {
+	      seconds = "0" + seconds;
+	    }
+      
+	  QString numString = mins + ":" + seconds;
+	  x = frameWidth + toInt((timePos-leftTime()) / (timeWidth() / double(w)));
+	  p.drawText(x - (p.fontMetrics().width(numString) / 2), textBottom, numString);
+	  p.drawLine(x, bigLineTop, x, bigLineBottom);
+	}
+      else
+	{
+	  //draw the smaller lines
+	  x = frameWidth + toInt((timePos - leftTime()) / (timeWidth() / double(w)));
+	  p.drawLine(x, smallLineTop, x, smallLineBottom);
+	}
+    }
   //draw the horizontal line
-  if(_numbersOnTop) {
-    p.drawLine(0, h-1, width(), h-1);
-  } else {
-    p.drawLine(0, 0, width(), 0);
-  }
+  if(_numbersOnTop)
+    {
+      p.drawLine(0, h - 1, width(), h - 1);
+    }
+  else
+    {
+      p.drawLine(0, 0, width(), 0);
+    }
   endDrawing();
 }
+
+// EOF
--- widgets/timeaxis.h
+++ widgets/timeaxis.h
@@ -4,6 +4,8 @@
     begin                : Mon Dec 13 2004
     copyright            : (C) 2004-2005 by Philip McLeod
     email                : pmcleod@cs.otago.ac.nz
+    copyright            : (C) 2016 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
  
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -28,33 +30,57 @@ class TimeAxis : public DrawWidget
 {
   Q_OBJECT
 
+public:
+  TimeAxis(QWidget *parent, bool numbersOnTop_=true);
+  TimeAxis(QWidget *parent, double leftTime_, double rightTime_, bool numbersOnTop_=true);
+  ~TimeAxis(void);
+
+  QSize sizeHint(void) const
+  {
+    return QSize(700, 20);
+  }
+
+  void init(void);
+  double leftTime(void)
+  {
+    return gdata->getView().viewLeft();
+  }
+  double rightTime(void)
+  {
+    return gdata->getView().viewRight();
+  }
+  double timeWidth(void)
+  {
+    return gdata->getView().viewTotalTime();
+  }
+
+  void setFontSize(int fontSize);
+  
+  void paintEvent(QPaintEvent *);
+
+  public slots:
+  void setLeftTime(double time)
+  {
+    _leftTime = time;
+  }
+  void setRightTime(double time)
+  {
+    _rightTime = time;
+  }
+  void setRange(double leftTime_, double rightTime_)
+  {
+    _leftTime = leftTime_;
+    _rightTime = rightTime_;
+  }
+
+ private:
   double _leftTime, _rightTime;
   QPixmap *buffer;
   bool _numbersOnTop;
   QFont _font;
   int _fontSize;
     
-public:
-  TimeAxis(QWidget *parent, bool numbersOnTop_=true);
-  TimeAxis(QWidget *parent, double leftTime_, double rightTime_, bool numbersOnTop_=true);
-  ~TimeAxis();
-
-  QSize sizeHint() const { return QSize(700, 20); }
-
-  void init();
-  double leftTime() { return gdata->getView().viewLeft(); }
-  double rightTime() { return gdata->getView().viewRight(); }
-  double timeWidth() { return gdata->getView().viewTotalTime(); }
-
-  void setFontSize(int fontSize);
-  
-  void paintEvent(QPaintEvent *);
-
-public slots:
-  void setLeftTime(double time) { _leftTime = time; }
-  void setRightTime(double time) { _rightTime = time; }
-  void setRange(double leftTime_, double rightTime_) { _leftTime = leftTime_, _rightTime = rightTime_; }
-
 };
 
-#endif
+#endif // TIMEAXIS_H
+// EOF
