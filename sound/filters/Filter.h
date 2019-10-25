0fcf6a94c171cf6138c6d09bf83633cd9f15d61c
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-10-25 13:43:07 +0200

    Apply naming conventions and cosmetic changes to filter

--- sound/filters/Filter.h
+++ sound/filters/Filter.h
@@ -4,7 +4,9 @@
     begin                : 2004
     copyright            : (C) 2004-2005 by Philip McLeod
     email                : pmcleod@cs.otago.ac.nz
- 
+    copyright            : (C) 2019 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
@@ -19,11 +21,14 @@ typedef unsigned int uint;
 
 class Filter
 {
-public:
-  virtual void filter(const float *input, float *output, int n) = 0;
-  virtual void reset(void) = 0;
-  virtual int delay(void) const { return 0; }
-  virtual inline ~Filter(void) { }
+  public:
+    virtual void filter( const float * p_input
+                       , float * p_output
+                       , int p_n
+                       ) = 0;
+    virtual void reset(void) = 0;
+    virtual int delay(void) const { return 0; }
+    virtual inline ~Filter(void) { }
 };
 
 #endif // FILTER_H
