e996fe7e6307d43396c3773f65ae6183855ee568
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-10-26 15:58:14 +0200

    Cosmetic changes to myassert

--- include/myassert.h
+++ include/myassert.h
@@ -4,7 +4,9 @@
     begin                : 2004
     copyright            : (C) 2004-2005 by Philip McLeod
     email                : pmcleod@cs.otago.ac.nz
- 
+    copyright            : (C) 2019 by Julien Thevenon
+    email                : julien_thevenon at yahoo.fr
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
@@ -13,23 +15,28 @@
    Please read LICENSE.txt for details.
  ***************************************************************************/
 
-#ifndef MYASSERT
-#define MYASSERT
+#ifndef MYASSERT_H
+#define MYASSERT_H
 
 #ifdef MYDEBUG // Cause a crash in debug mode so you can trace it with your debugger
 
-#define myassert(expr) if(!(expr)) { \
-  fprintf(stderr, "Assert Failed: %s in %s line %u.\nCausing a trace crash.\n", #expr, __FILE__, __LINE__); \
-  fflush(stderr); \
-  *((int*)0) = 0; } //cause a crash
+#define myassert(expr) if(!(expr))                                                                              \
+  {                                                                                                             \
+      fprintf(stderr, "Assert Failed: %s in %s line %u.\nCausing a trace crash.\n", #expr, __FILE__, __LINE__); \
+      fflush(stderr);                                                                                           \
+      *((int*)0) = 0;                                                                                           \
+  } //cause a crash
 
 #else // Do nothing when not in debug mode
 #define myassert(expr) ;
-#endif
+#endif // MYDEBUG
 
-#define myAlwaysAssert(expr) if(!(expr)) { \
-  fprintf(stderr, "Assert Failed: %s in %s line %u.\nCausing a trace crash.\n", #expr, __FILE__, __LINE__); \
-  fflush(stderr); \
-  *((int*)0) = 0; } //cause a crash
+#define myAlwaysAssert(expr) if(!(expr))                                                                        \
+  {                                                                                                             \
+      fprintf(stderr, "Assert Failed: %s in %s line %u.\nCausing a trace crash.\n", #expr, __FILE__, __LINE__); \
+      fflush(stderr);                                                                                           \
+      *((int*)0) = 0;                                                                                           \
+  } //cause a crash
 
-#endif //MYASSERT
+#endif //MYASSERT_H
+// EOF
