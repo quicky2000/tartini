465a203f84f42a6a6dab08dc4523a7b144af5d80
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-10-26 10:38:43 +0200

    Apply naming conventions to smartPtr

--- include/SmartPtr.h
+++ include/SmartPtr.h
@@ -36,40 +36,40 @@ class SmartPtr {
     typedef unsigned int                       map_data_type;
 
  private:
-    T *_ptr; //Pointer to the actual object, or NULL
-    map_data_type *_ref_counter; //Pointer to the reference counter in the map
+    T *m_ptr; //Pointer to the actual object, or NULL
+    map_data_type *m_ref_counter; //Pointer to the reference counter in the map
 
-    static map_type _smartPtrs;
+    static map_type m_smart_ptrs;
 
  public:
     /** Default constructor
 	Speed: Constant time
      */
-    SmartPtr() :  _ptr(NULL), _ref_counter(NULL) { }
+    SmartPtr() : m_ptr(NULL), m_ref_counter(NULL) { }
     /** Construct from pointer
 	Speed: Logarithmic to the size of the map
      */
-    SmartPtr(T *ptr) : _ptr(ptr) {
-	if(_ptr) _ref_counter = &(++(_smartPtrs[_ptr]));
-	else _ref_counter = NULL;
+    SmartPtr(T *p_ptr) : m_ptr(p_ptr) {
+	if(m_ptr) m_ref_counter = &(++(m_smart_ptrs[m_ptr]));
+	else m_ref_counter = NULL;
     }
     /** Construct from SmartPtr
 	Speed: Constant time
      */
-    SmartPtr(SmartPtr<T> const &r) : _ptr(r.ptr()) {
-	if(_ptr) ++(*(_ref_counter = r.getRefCounter())); //no need to search the map here
-	else _ref_counter = NULL;
+    SmartPtr(SmartPtr<T> const &r) : m_ptr(r.ptr()) {
+	if(m_ptr) ++(*(m_ref_counter = r.getRefCounter())); //no need to search the map here
+	else m_ref_counter = NULL;
     }
     /** Destructor
 	Speed: Logarithmic if last reference, else Constant time
      */
     ~SmartPtr() {
-	if(_ptr) {
-	    if(--(*_ref_counter) == 0) {
-		_smartPtrs.erase(_ptr); //we need to search the map here to erase the element
-		delete _ptr;
-		_ptr = NULL;
-		_ref_counter = NULL;
+	if(m_ptr) {
+	    if(--(*m_ref_counter) == 0) {
+            m_smart_ptrs.erase(m_ptr); //we need to search the map here to erase the element
+		delete m_ptr;
+            m_ptr = NULL;
+            m_ref_counter = NULL;
 	    }
 	}
     }
@@ -77,22 +77,22 @@ class SmartPtr {
 	Note: Creates the new pointer before deleting the old pointer
 	Speed: Constant time, unless previous value was a last reference, then logarithmic
     */
-    SmartPtr<T> &operator = (SmartPtr<T> const &r) {
-	if(_ptr != r.ptr()) {
-	    T *old_ptr = _ptr;
-	    map_data_type *old_ref_counter = _ref_counter;
+    SmartPtr<T> &operator =(SmartPtr<T> const &p_r) {
+	if(m_ptr != p_r.ptr()) {
+	    T *l_old_ptr = m_ptr;
+	    map_data_type *l_old_ref_counter = m_ref_counter;
 
-	    if(r.ptr()) {
-		_ptr = r.ptr();
-		++(*(_ref_counter = r.getRefCounter()));
+	    if(p_r.ptr()) {
+            m_ptr = p_r.ptr();
+		++(*(m_ref_counter = p_r.getRefCounter()));
 	    } else {
-		_ptr = NULL;
-		_ref_counter = NULL;
+            m_ptr = NULL;
+            m_ref_counter = NULL;
 	    }
-	    if(old_ptr) {
-		if(--(*old_ref_counter) == 0) {
-		    _smartPtrs.erase(old_ptr);
-		    delete old_ptr;
+	    if(l_old_ptr) {
+		if(--(*l_old_ref_counter) == 0) {
+            m_smart_ptrs.erase(l_old_ptr);
+		    delete l_old_ptr;
 		}
 	    }
 	}
@@ -101,22 +101,22 @@ class SmartPtr {
     /** Assignment of a raw pointer (Shallow copy)
 	Speed: Logarithmic to the size of the map
      */
-    SmartPtr<T> &operator = (T *p) {
-	if(_ptr != p) {
-	    T *old_ptr = _ptr;
-	    map_data_type *old_ref_counter = _ref_counter;
+    SmartPtr<T> &operator =(T *p_p) {
+	if(m_ptr != p_p) {
+	    T *l_old_ptr = m_ptr;
+	    map_data_type *l_old_ref_counter = m_ref_counter;
 
-	    if(p) {
-		_ref_counter = &(++(_smartPtrs[p])); //this is the magic!
-		_ptr = p;
+	    if(p_p) {
+            m_ref_counter = &(++(m_smart_ptrs[p_p])); //this is the magic!
+		m_ptr = p_p;
 	    } else {
-		_ptr = NULL;
-		_ref_counter = NULL;
+            m_ptr = NULL;
+            m_ref_counter = NULL;
 	    }
-	    if(old_ptr) {
-		if(--(*old_ref_counter) == 0) {
-		    _smartPtrs.erase(old_ptr);
-		    delete old_ptr;
+	    if(l_old_ptr) {
+		if(--(*l_old_ref_counter) == 0) {
+            m_smart_ptrs.erase(l_old_ptr);
+		    delete l_old_ptr;
 		}
 	    }
 	}	
@@ -127,77 +127,77 @@ class SmartPtr {
 	Speed: Logarithmic
      */
     SmartPtr<T> copy() {
-	T* t = new T();
-	*t = *_ptr;
-	return SmartPtr<T>(t);
+	T* l_t = new T();
+	*l_t = *m_ptr;
+	return SmartPtr<T>(l_t);
     }
 
     /** boolean (cast) operator. e.g. if(smartPtr) ... */
-    operator safe_bool () const { return (_ptr != (T*)NULL) ? SPTR_SAFE_BOOL_TRUE : false; }
+    operator safe_bool () const { return (m_ptr != (T*)NULL) ? SPTR_SAFE_BOOL_TRUE : false; }
     /** boolean not operator. e.g. if(!smartPtr) ... */
-    bool operator !() const { return (_ptr != (T*)NULL) ? false : true; }
+    bool operator !() const { return (m_ptr != (T*)NULL) ? false : true; }
     /** Dereferencing the pointer */
-    T &operator * () const { return *_ptr; }
+    T &operator * () const { return *m_ptr; }
     /** Return the raw/dumb pointer. */
-    T *operator -> () const { return _ptr; }
+    T *operator -> () const { return m_ptr; }
     /** Return the raw/dumb pointer. */
-    T *ptr() const { return _ptr; }
-    //operator T *() const { return _ptr; } //People should explicitly get the pointer using ptr() 
+    T *ptr() const { return m_ptr; }
+    //operator T *() const { return m_ptr; } //People should explicitly get the pointer using ptr()
 
    /** Do they point to the same object. (shallow equality) */
-   bool operator == (T const *p) const { return _ptr == p; }
+   bool operator ==(T const *p_p) const { return m_ptr == p_p; }
  
    /** Do the smart pointers not point to the same object. (shallow inequality) */
-   bool operator != (T const *p) const { return _ptr != p; }
+   bool operator !=(T const *p_p) const { return m_ptr != p_p; }
  
    /** Do they point to the same object. (shallow equality) */
-    bool operator == (SmartPtr<T> const &r) const { return _ptr == r.ptr(); }
+    bool operator ==(SmartPtr<T> const &p_r) const { return m_ptr == p_r.ptr(); }
 
     /** Do the smart pointers not point to the same object. (shallow inequality) */
-    bool operator != (SmartPtr<T> const &r) const { return _ptr != r.ptr(); }
-    bool operator <  (SmartPtr<T> const &r) const { return _ptr <  r.ptr(); }
-    bool operator >  (SmartPtr<T> const &r) const { return _ptr >  r.ptr(); }
-    bool operator <= (SmartPtr<T> const &r) const { return _ptr <= r.ptr(); }
-    bool operator >= (SmartPtr<T> const &r) const { return _ptr >= r.ptr(); }
+    bool operator !=(SmartPtr<T> const &p_r) const { return m_ptr != p_r.ptr(); }
+    bool operator <(SmartPtr<T> const &p_r) const { return m_ptr < p_r.ptr(); }
+    bool operator >(SmartPtr<T> const &p_r) const { return m_ptr > p_r.ptr(); }
+    bool operator <=(SmartPtr<T> const &p_r) const { return m_ptr <= p_r.ptr(); }
+    bool operator >=(SmartPtr<T> const &p_r) const { return m_ptr >= p_r.ptr(); }
 
     /* Returns the number of references to this object
        Speed: Constant time
      */
-    unsigned int getNumRef() { return (_ptr) ? *_ref_counter : 0; }
+    unsigned int getNumRef() { return (m_ptr) ? *m_ref_counter : 0; }
     /** This allows read access to the map, to see what memory is being used
 	Mainly for debugging purposes
      */
-    static const map_type &getMap() { return _smartPtrs; }
+    static const map_type &getMap() { return m_smart_ptrs; }
 
 protected:
-    unsigned int *getRefCounter() const { return _ref_counter; }
+    unsigned int *getRefCounter() const { return m_ref_counter; }
 };
 
 
 /** Is p and r pointing to the same object */
 //: Comparison of pointer with smart-pointer (cannot be a member function)
 template <class T>
-inline bool operator == (T const* p, SmartPtr<T> const& r)
+inline bool operator ==(T const* p_p, SmartPtr<T> const& p_r)
 {
-    return r.ptr() == p;
+    return p_r.ptr() == p_p;
 }
 
 /** Is p and r pointing to the same object */
 template <class T>
-inline bool operator!= (T const* p, SmartPtr<T> const& r)
+inline bool operator!=(T const* p_p, SmartPtr<T> const& p_r)
 {
-    return r.ptr() != p;
+    return p_r.ptr() != p_p;
 }
 
 /** This inforces instatiation of the static variable _smartPtrs */
-template <class T> std::map<const T*, unsigned int> SmartPtr<T>::_smartPtrs = std::map<const T*, unsigned int>();
+template <class T> std::map<const T*, unsigned int> SmartPtr<T>::m_smart_ptrs = std::map<const T*, unsigned int>();
 
 /** Test whether a pointer has been turn into a smart pointer
     Speed: Logarithmic
  */
 template <class T>
-inline bool isSmart(const T* ptr) {
-    return (ptr) ? SmartPtr<T>::getMap().count(ptr) : false;
+inline bool isSmart(const T* p_ptr) {
+    return (p_ptr) ? SmartPtr<T>::getMap().count(p_ptr) : false;
 }
 
 #endif
