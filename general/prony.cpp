b91d2ef5fd2fd43163028132d42fdad483baab28
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-10-29 16:09:46 +0100

    Cosmetic changes to prony

--- general/prony.cpp
+++ general/prony.cpp
@@ -25,7 +25,12 @@
 #include "array1d.h"
 
 //------------------------------------------------------------------------------
-bool pronyFit(PronyData *p_result, const float *p_x, int p_length, int p_gap, bool p_allow_offset)
+bool pronyFit( PronyData * p_result
+             , const float * p_x
+             , int p_length
+             , int p_gap
+             , bool p_allow_offset
+             )
 {
     myassert(p_result != NULL);
     myassert(p_x != NULL);
@@ -39,61 +44,61 @@ bool pronyFit(PronyData *p_result, const float *p_x, int p_length, int p_gap, bo
     int l_gap2 = p_gap * 2;
     int l_n = p_length - l_gap2;
     for(l_j = 0; l_j < l_n; l_j++)
-      {
+    {
           l_x1[l_j] = p_x[l_j] + p_x[l_j + l_gap2];
-      }
+    }
     if(p_allow_offset)
-      {
-	if(!pinv(NULL, p_x + p_gap, l_x1.begin(), l_n, l_alpha))
-	  {
-	    return false;
-	  }
-          l_omega = acos(l_alpha[1] / 2) / p_gap;
-      }
+    {
+        if(!pinv(NULL, p_x + p_gap, l_x1.begin(), l_n, l_alpha))
+        {
+            return false;
+        }
+        l_omega = acos(l_alpha[1] / 2) / p_gap;
+    }
     else
-      {
-	if(!pinv(p_x + p_gap, l_x1.begin(), l_n, l_alpha))
-	  {
-	    return false;
-	  }
-          l_omega = acos(l_alpha[0] / 2) / p_gap;
-      }
+    {
+        if(!pinv(p_x + p_gap, l_x1.begin(), l_n, l_alpha))
+        {
+            return false;
+        }
+        l_omega = acos(l_alpha[0] / 2) / p_gap;
+    }
     if(std::isnan(l_omega))
-      {
-	return false;
-      }
+    {
+        return false;
+    }
 
     p_result->m_omega = l_omega;
     for(l_j = 0; l_j < p_length; l_j++)
-      {
+    {
           l_x1[l_j] = cos(l_j * l_omega);
           l_x2[l_j] = sin(l_j * l_omega);
-      }
+    }
     if(p_allow_offset)
-      {
-	if(!pinv(NULL, l_x1.begin(), l_x2.begin(), p_x, p_length, l_alpha))
-	  {
-	    return false;
-	  }
-          p_result->m_y_offset = l_alpha[0];
-          p_result->m_amp = hypot(l_alpha[1], l_alpha[2]);
-          p_result->m_phase = (g_half_pi - atan2(l_alpha[2], l_alpha[1]));
-      }
+    {
+        if(!pinv(NULL, l_x1.begin(), l_x2.begin(), p_x, p_length, l_alpha))
+        {
+            return false;
+        }
+        p_result->m_y_offset = l_alpha[0];
+        p_result->m_amp = hypot(l_alpha[1], l_alpha[2]);
+        p_result->m_phase = (g_half_pi - atan2(l_alpha[2], l_alpha[1]));
+    }
     else
-      {
-	if(!pinv(l_x1.begin(), l_x2.begin(), p_x, p_length, l_alpha))
-	  {
-	    return false;
-	  }
-          p_result->m_amp = hypot(l_alpha[0], l_alpha[1]);
-          p_result->m_phase = (g_half_pi - atan2(l_alpha[1], l_alpha[0]));
-      }
+    {
+        if(!pinv(l_x1.begin(), l_x2.begin(), p_x, p_length, l_alpha))
+        {
+            return false;
+        }
+        p_result->m_amp = hypot(l_alpha[0], l_alpha[1]);
+        p_result->m_phase = (g_half_pi - atan2(l_alpha[1], l_alpha[0]));
+    }
 
     //calculate the squared error
     for(l_j = 0; l_j < p_length; l_j++)
-      {
-	l_error += sq(p_result->m_amp * sin(l_j * l_omega + p_result->m_phase) + p_result->m_y_offset - p_x[l_j]);
-      }
+    {
+        l_error += sq(p_result->m_amp * sin(l_j * l_omega + p_result->m_phase) + p_result->m_y_offset - p_x[l_j]);
+    }
     p_result->m_error = l_error / p_length;
 
     return true;
--- general/prony.h
+++ general/prony.h
@@ -24,45 +24,49 @@ const double g_half_pi = 3.1415926535897932384 * 0.5;
 */
 struct PronyData
 { 
-  /**
-     amplitude
-  */
-  double m_amp;
+    /**
+       amplitude
+    */
+    double m_amp;
 
-  /**
-     phase in radians
-   */
-  double m_phase;
+    /**
+       phase in radians
+     */
+    double m_phase;
 
-  /**
-     frequency in radians per unit
-  */
-  double m_omega;
+    /**
+       frequency in radians per unit
+    */
+    double m_omega;
 
-  double m_y_offset;
+    double m_y_offset;
 
-  /**
-     squared error of the fit
-  */
-  double m_error;
+    /**
+       squared error of the fit
+    */
+    double m_error;
 
-  PronyData(void)
-  {
-    reset();
-  }
+    PronyData(void)
+    {
+        reset();
+    }
   
-  void reset(void)
-  {
-      m_amp = m_phase = m_omega = m_y_offset = m_error = 0.0;
-  }
+    void reset(void)
+    {
+        m_amp = 0.0;
+        m_phase = 0.0;
+        m_omega = 0.0;
+        m_y_offset = 0.0;
+        m_error = 0.0;
+    }
 
-  /**
-     @param p_dt is the time between samples in the input.
-  */
-  double freqHz(double p_dt)
-  {
-    return m_omega / (p_dt * g_two_pi);
-  }
+    /**
+       @param p_dt is the time between samples in the input.
+    */
+    double freqHz(double p_dt)
+    {
+        return m_omega / (p_dt * g_two_pi);
+    }
 };
 
 /**
@@ -73,7 +77,12 @@ struct PronyData
    @param p_gap The number samples used as a delay size (best is around 1/4 of a cycle)
    @return true if the prony fit was a success. else false 
 */
-bool pronyFit(PronyData *p_result, const float *p_x, int p_length, int p_gap, bool p_allow_offset);
+bool pronyFit( PronyData * p_result
+             , const float * p_x
+             , int p_length
+             , int p_gap
+             , bool p_allow_offset
+             );
 
 #endif // PRONY_H
 // EOF
