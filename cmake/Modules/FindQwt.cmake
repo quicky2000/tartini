3a7c0aa7cccda56cddda01eb8ef34419c94711d7
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2019-11-18 17:22:24 +0100

    Improve management of Qwt on OSx

--- cmake/Modules/FindQwt.cmake
+++ cmake/Modules/FindQwt.cmake
@@ -45,7 +45,7 @@ if("${PC_QWT}" STREQUAL "")
               NAMES qwt.h
               PATHS ${QWT_PATHS}
               HINTS ${QWT_HINTS}
-              PATH_SUFFIXES include qwt-qt4 qwt-qt5 qwt
+              PATH_SUFFIXES include qwt-qt4 qwt-qt5 qwt Headers
               DOC "Variable storing the location of Qwt header"
              )
 
@@ -60,8 +60,6 @@ if("${PC_QWT}" STREQUAL "")
                  HINTS ${QWT_HINTS}
                  PATH_SUFFIXES ${ARCH_SUFFIX}
                  DOC "Variable storing the location of Qwt library"
-                 NO_DEFAULT_PATH
-                 NO_CMAKE_PATH
                 )
 
     set(Qwt_VERSION ${Qwt_FIND_VERSION})
@@ -80,10 +78,22 @@ if("${PC_QWT}" STREQUAL "")
         #set(Qwt_DEFINITIONS ${PC_Qwt_CFLAGS_OTHER})
     endif()
     if(Qwt_FOUND AND NOT TARGET Qwt::Qwt)
-        add_library(Qwt::Qwt UNKNOWN IMPORTED)
-        set_target_properties(Qwt::Qwt PROPERTIES
-        IMPORTED_LOCATION "${Qwt_LIBRARY}"
-        INTERFACE_COMPILE_OPTIONS ""
-        INTERFACE_INCLUDE_DIRECTORIES "${Qwt_INCLUDE_DIR}")
+        if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
+            get_filename_component(FRAMEWORK_LOC ${Qwt_LIBRARY} DIRECTORY)
+            add_library(Qwt::Qwt INTERFACE IMPORTED)
+            set_target_properties(Qwt::Qwt PROPERTIES
+                                  INTERFACE_COMPILE_OPTIONS ""
+                                  INTERFACE_INCLUDE_DIRECTORIES "${Qwt_INCLUDE_DIR}"
+                                  )
+
+            target_link_libraries(Qwt::Qwt INTERFACE "-F${FRAMEWORK_LOC} -framework qwt")
+        else("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
+            add_library(Qwt::Qwt UNKNOWN IMPORTED)
+            set_target_properties(Qwt::Qwt PROPERTIES
+                                  IMPORTED_LOCATION "${Qwt_LIBRARIES}"
+                                  INTERFACE_COMPILE_OPTIONS ""
+                                  INTERFACE_INCLUDE_DIRECTORIES "${Qwt_INCLUDE_DIR}"
+                                  )
+        endif("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
     endif()
 endif("${PC_QWT}" STREQUAL "")
