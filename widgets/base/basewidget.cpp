e7518102e3a905928e481ec0964ad56d1bfaf64f
Matthew Self <dmz@mself.com>
2020-05-18 23:25:43 -0700

    Restore widgets/base/* source files
    These were previously removed as unused

---
+++ widgets/base/basewidget.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+                          basewidget.cpp  -  description
+                             -------------------
+    begin                : May 18 2005
+    copyright            : (C) 2005 by Philip McLeod
+    email                : pmcleod@cs.otago.ac.nz
+ 
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   Please read LICENSE.txt for details.
+ ***************************************************************************/
+#include <QPixmap>
+#include <QPainter>
+
+#include "basewidget.h"
+#include "gdata.h"
+#include "channel.h"
+#include "analysisdata.h"
+#include "useful.h"
+
+//------------------------------------------------------------------------------
+BaseWidget::BaseWidget(QWidget * p_parent)
+: DrawWidget(p_parent)
+{
+}
+
+//------------------------------------------------------------------------------
+BaseWidget::~BaseWidget()
+{
+}
+
+//------------------------------------------------------------------------------
+void BaseWidget::paintEvent( QPaintEvent * )
+{
+    Channel * l_active_channel = gdata->getActiveChannel();
+
+    beginDrawing();
+
+    // Drawing code goes here
+    p.drawLine(0, 0, width(), height());
+
+    endDrawing();
+}
+
+//------------------------------------------------------------------------------
+QSize BaseWidget::sizeHint() const
+{
+    return QSize(300, 200);
+}
+
+// EOF
