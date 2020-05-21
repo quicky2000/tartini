quicky2000/tartini

commit ae91001e175f84e78254141f5644f11b6fb777f4

Add override keyword to enforce cimpiler checks on virtual methods

sound/sound_file_stream.h
@@ -25,33 +25,33 @@ class SoundFileStream : public SoundStream
   public:
 
     inline SoundFileStream();
-    virtual inline ~SoundFileStream();
+    inline ~SoundFileStream() override;
 
     inline int data_length() const;
     inline int totalFrames() const;
     inline int pos() const;
   
     virtual inline int open_read(const std::string & /*filename*/);
-    virtual inline long read_bytes( void * /*data*/
-                                  , long /*length*/
-                                  );
-    virtual inline long read_frames( void * /*data*/
-                                   , long /*length*/
-                                   );
+    inline long read_bytes( void * /*data*/
+                          , long /*length*/
+                          ) override;
+    inline long read_frames( void * /*data*/
+                           , long /*length*/
+                           ) override;
 
-    virtual inline int open_write( const std::string & /*filename*/
-                                 , int /*freq_*/ = 44100
-                                 , int /*channels_*/ = 2
-                                 , int /*bits_*/ = 16
-                                 );
+    inline int open_write( const std::string & /*filename*/
+                         , int /*freq_*/ = 44100
+                         , int /*channels_*/ = 2
+                         , int /*bits_*/ = 16
+                         );
 
-    virtual inline long write_bytes( void * /*data*/
-                                   , long /*length*/
-                                   );
+    inline long write_bytes( void * /*data*/
+                           , long /*length*/
+                           ) override;
 
-    virtual inline long write_frames( void * /*data*/
-                                    , long /*length*/
-                                    );
+    inline long write_frames( void * /*data*/
+                            , long /*length*/
+                            ) override;
 
     virtual inline void close();
 

sound/wave_stream.h
@@ -24,16 +24,16 @@ class WaveStream : public SoundFileStream
 {
   public:
     WaveStream();
-    virtual ~WaveStream();
+    ~WaveStream() override;
 
-    int open_read(const std::string & p_filename);
+    int open_read(const std::string & p_filename) override;
     int read_header();
     long read_bytes( void * p_data
                    , long p_length
-                   );
+                   ) override;
     long read_frames( void * p_data
                     , long p_length
-                    );
+                    ) override;
 
     int open_write( const std::string & p_filename
                   , int p_freq = 44100
@@ -45,19 +45,19 @@ class WaveStream : public SoundFileStream
 
     long write_bytes( void * p_data
                     , long p_length
-                    );
+                    ) override;
 
     long write_frames( void * p_data
                      , long p_length
-                     );
+                     ) override;
 
     QStringList getOutputDeviceNames();
 
-    void close();
+    void close() override;
 
-    void jump_to_frame(int p_frame);
-    void jump_back(int p_frames);
-    void jump_forward(int p_frames);
+    void jump_to_frame(int p_frame) override;
+    void jump_back(int p_frames) override;
+    void jump_forward(int p_frames) override;
 
   private:
     FILE * m_file;

commit c9143516b812f4fd0b8e39ea0c1f3e5595763f3d

Fix for crash introduced in commit `ae91001`
The `virtual` keyword was accidently removed from `SoundFileStream::open_write()`
- Restore `virtual` keyword to `SoundFileStream::open_write()`
- Add `override` keyword to `WaveStream::open_write()`

sound/sound_file_stream.h
@@ -39,7 +39,7 @@ class SoundFileStream : public SoundStream
                            , long /*length*/
                            ) override;
 
-    inline int open_write( const std::string & /*filename*/
+    virtual inline int open_write( const std::string & /*filename*/
                          , int /*freq_*/ = 44100
                          , int /*channels_*/ = 2
                          , int /*bits_*/ = 16

sound/wave_stream.h
@@ -39,7 +39,7 @@ class WaveStream : public SoundFileStream
                   , int p_freq = 44100
                   , int p_channels = 2
                   , int p_bits = 16
-                  );
+                  ) override;
 
     void write_header();
 